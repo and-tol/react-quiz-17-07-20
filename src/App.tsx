d3151967316d8a7ce5ee262099982717c39d9623
and-tol <tolstoi.a@gmail.com>
2020-07-17 17:19:09 +0300

    feat:App add hooks for props of QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+// Components
+import { QuestionCard } from './components/QuestionCard/QuestionCard';
 
-function App() {
+const TOTAL_QUESTIONS = 10;
+
+const App = () => {
+  const [loading, setLoding] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [useAnswers, setUseAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {};
+
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
-    <div className="App">
-     Quiz
+    <div className='App'>
+      <h1>React Quiz</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading question...</p>
+      <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={useAnswers ? useAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className='next' onClick={nextQuestion}>
+        Next Qustion
+      </button>
     </div>
   );
-}
+};
 
 export default App;
