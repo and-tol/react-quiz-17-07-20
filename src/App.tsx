575215742ca04e48e9cc32ace81ef278aa7f52ab
and-tol <tolstoi.a@gmail.com>
2020-07-18 16:23:47 +0300

    feat: implement check answer, move to next question

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,106 @@
 import React, { useState } from 'react';
-import {fetchQuizQuestion} from './API'
+import { fetchQuizQuestion } from './API';
 // Components
 import { QuestionCard } from './components/QuestionCard/QuestionCard';
+// Types
+import { QuestionState, Difficulty } from './API';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
-  const [loading, setLoding] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [useAnswers, setUseAnswers] = useState([]);
+  const [useAnswers, setUseAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  const startTrivia = async () => {};
+  console.log(questions);
 
-  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {};
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
 
-  const nextQuestion = () => {};
+    const newQuestions = await fetchQuizQuestion(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUseAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      // User answer
+      const answer = event.currentTarget.value;
+      // Check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      // Add scrore if answer is correct
+      if (correct) {
+        setScore((prev) => prev + 1);
+      }
+      // Save answer in the array for the user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+
+      setUseAnswers((prev) => [...prev, answerObject]);
+    }
+  };
+
+  const nextQuestion = () => {
+    // Move on to next question if not the last question
+    const nextQuestion = number + 1
+    
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true)
+    } else {
+      setNumber(nextQuestion)
+    }
+  };
 
   return (
     <div className='App'>
       <h1>React Quiz</h1>
-      <button className='start' onClick={startTrivia}>
-        Start
-      </button>
-      <p className='score'>Score:</p>
-      <p>Loading question...</p>
-      {/* <QuestionCard
-        questionNr={number + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[number].question}
-        answers={questions[number].answers}
-        userAnswer={useAnswers ? useAnswers[number] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className='next' onClick={nextQuestion}>
-        Next Qustion
-      </button>
+      {gameOver || useAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className='score'>Score:</p> : null}
+      {loading && <p>Loading question...</p>}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={useAnswers ? useAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {!gameOver &&
+      !loading &&
+      useAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className='next' onClick={nextQuestion}>
+          Next Qustion
+        </button>
+      ) : null}
     </div>
   );
 };
